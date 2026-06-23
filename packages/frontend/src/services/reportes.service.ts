e457273873a37b68555da9811039e3dd5ccd41d9
sanguinettiluca <lucasanguinettical14@gmail.com>
2026-06-23 00:49:18 -0300

    fix: tipo Estadisticas clientesAcumuladosPorMes

--- packages/frontend/src/services/reportes.service.ts
+++ packages/frontend/src/services/reportes.service.ts
@@ -1,11 +1,11 @@
 import api from "./api";
 
-export interface Estadisticas{
-    polizasActivasPorCompania: {nombre: string, cantidad: number}[];
-    clientesAcumuladosPorMes: {mes: string, cantidad: number}[];
+export interface Estadisticas {
+  polizasActivasPorCompania: { nombre: string; cantidad: number }[];
+  clientesAcumuladosPorMes: { mes: string; total: number }[];
 }
 
 export async function obtenerEstadisticas(): Promise<Estadisticas> {
-    const {data} = await api.get("/reportes/estadisticas");
-    return data;
+  const { data } = await api.get("/reportes/estadisticas");
+  return data;
 }
